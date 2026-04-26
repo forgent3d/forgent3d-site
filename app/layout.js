d09f7de830f741ea64cc8b016bb819cc0298c2a2
coco-forgent <meihuanyu88@gmail.com>
2026-04-26 14:34:44 +0800

    Update favicon and site manifest: replace SVG favicon with PNG icons, and add Android Chrome icons to the web manifest.

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,9 @@ export default function RootLayout({ children }) {
         <meta property="og:type" content="website" />
         <meta property="og:image" content="/og-image.svg" />
         <meta property="og:locale" content="en_US" />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
